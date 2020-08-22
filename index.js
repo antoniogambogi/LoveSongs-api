37e8fae78d79a84dbaf9ee9bfd68b256040ed7e3
antoniogambogi <antoniogambogi88@gmail.com>
2020-08-21 22:23:31 -0300

    Configurações iniciais do nosso servidor de backend para a API

---
+++ index.js
@@ -0,0 +1,29 @@
+// Importando dependências necessárias para rodar a API.
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+// Configurando o body parser da API.
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.text())
+app.use(bodyParser.json({type: 'application/json'}))
+
+// Configurando o CORS da API.
+app.use(cors())
+
+// Configurando cabeçalhos de response padrão.
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+// Configurando o endpoint / para responder um JSON com uma mensagem.
+app.get('/', (req, res) => {
+    res.send({message: `API LoveSongs tocando na porta ${PORT}`})
+})
+
+// Iniciando o servidor da API na porta configurada na varíavel de ambiente ou 3000.
+app.listen(PORT, () => console.log(`API LoveSongs tocando na porta ${PORT}`))
