de3dcd2048f71f93d039ea30c768e986887fbf21
antoniogambogi <antoniogambogi88@gmail.com>
2020-08-22 20:05:13 -0300

    Criando o arquivo de rota para criar música

---
+++ src/app/routes/musicas.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const route = express.Router()
+const Musica = require('./../controllers/musicas.controller')
+
+route.post('criar', Musica.criarMusica)
+
+module.exports = route
