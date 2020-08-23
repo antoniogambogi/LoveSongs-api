377ee2c5b3a16a2d89c8264f08480ff1f90fb888
antoniogambogi <antoniogambogi88@gmail.com>
2020-08-22 21:47:22 -0300

    Adicionando rota visualizar todas músicas para visualizar todas as muisicas

--- src/app/routes/musicas.routes.js
+++ src/app/routes/musicas.routes.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const Musica = require('./../controllers/musicas.controller')
 
 route.post('/criar', Musica.criarMusica)
+route.get('/visualizarTodas', Musica.visualizarMusicas)
 
 module.exports = route
