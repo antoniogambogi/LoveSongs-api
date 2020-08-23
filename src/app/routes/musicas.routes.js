20e895660ff24a7c932c4afe9c51d6a8478ca3d9
antoniogambogi <antoniogambogi88@gmail.com>
2020-08-23 01:03:23 -0300

    Adicionando rota para atualizar uma música

--- src/app/routes/musicas.routes.js
+++ src/app/routes/musicas.routes.js
@@ -5,5 +5,6 @@ const Musica = require('./../controllers/musicas.controller')
 route.post('/criar', Musica.criarMusica)
 route.get('/visualizarTodas', Musica.visualizarMusicas)
 route.get('/visualizarUma/:musica', Musica.visualizarUmaMusica)
+route.put('/atualizarUma/:musica', Musica.atualizarUmaMusica)
 
 module.exports = route
