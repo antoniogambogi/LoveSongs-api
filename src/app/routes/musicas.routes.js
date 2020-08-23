9f7c14badb4f3d418a4823c21e81c0601195bc62
antoniogambogi <antoniogambogi88@gmail.com>
2020-08-22 22:18:08 -0300

    Liberando rota visualizar uma música para visualizar uma música

--- src/app/routes/musicas.routes.js
+++ src/app/routes/musicas.routes.js
@@ -4,5 +4,6 @@ const Musica = require('./../controllers/musicas.controller')
 
 route.post('/criar', Musica.criarMusica)
 route.get('/visualizarTodas', Musica.visualizarMusicas)
+route.get('/visualizarUma/:musica', Musica.visualizarUmaMusica)
 
 module.exports = route
