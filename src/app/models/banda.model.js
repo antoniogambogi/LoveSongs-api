4c4d433a75e88a0ad00f367b47340a5f567db593
antoniogambogi <antoniogambogi88@gmail.com>
2020-09-08 20:06:20 -0300

    Atualizando model de banda

---
+++ src/app/models/banda.model.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose')
+
+const BandaSchema = new Schema({
+    nome: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+        trim: true
+    },
+
+    genero: {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        trim: true
+    },
+
+    imagem: {
+        type: String,
+        trim: true
+    },
+
+    musicas: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Musica'
+        }
+    ]
+},
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+module.exports = model('Banda', BandaSchema)
