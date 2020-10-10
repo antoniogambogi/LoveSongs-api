3cb243d9c1270a09a71dada08e0db5574f763437
antoniogambogi <antoniogambogi88@gmail.com>
2020-10-10 19:01:24 -0300

    Implementando a opção delete de músicas

--- src/app/controllers/musica.controller.js
+++ src/app/controllers/musica.controller.js
@@ -90,5 +90,32 @@ class Musica {
         })
     }
 
+    // aqui sera o update de musicas
+
+    delete(req, res) {
+        const { songId } = req.params
+
+        banda.findOne({ musicas: songId }, (err, banda) => {
+            if (err) {
+                res.status(500).send({ message: "Houve um erro ao procfessar a sua requisição", error: err })
+            } else {
+                banda.musicas.pull(songId)
+                banda.save((err) => {
+                    if (err) {
+                        res.status(500).send({ message: "Houve um erro ao procfessar a sua requisição", error: err })
+                    }else{
+                        musica.deleteOne({_id: songId}, (err, result) => {
+                            if(err){
+                                res.status(500).send({ message: "Houve um erro ao procfessar a sua requisição", error: err })
+                            }else{
+                                res.status(200).send({message: "A música foi apagada com sucesso", data: result})
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+
 }
 module.exports = new Musica()
--- src/app/routes/musica.routes.js
+++ src/app/routes/musica.routes.js
@@ -6,5 +6,6 @@ route.post('/criar', Musica.criarMusica)
 route.get('/listarTodas', Musica.buscarTodasAsMusicas)
 route.get('/listarUma/:nomeMusica', Musica.buscarUmaMusicaPeloNome)
 route.get('/validarNomeMusica', Musica.validarNomeMusica)
+route.delete('/apagar/:songId', Musica.delete)
 
 module.exports = route
